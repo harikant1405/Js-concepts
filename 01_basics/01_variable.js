8723141d4b26468e94bd8fff8e3d2f9247ae88fa
Harikant <43916435+harikant1405@users.noreply.github.com>
2024-11-23 14:48:48 +0000

    basics of js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accId = 12345;
+let cityName = "jaipur";
+var address = "205, ARG"
+
+
+console.log(accId);
+console.table([accId,cityName,address]);
+ 
+/*
+let and const has block scope
+prefer not use var
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+
+/* 
+string = ""
+number => 2 to power 53
+null
+undefined
+boolean => true/false
+symbol => unique
+
+object
+
+*/
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
